120910659f479dd533a2178aa241c0c426781df1
mskim-99 <kms99400@gmail.com>
2025-12-08 17:56:22 +0900

    axios

---
+++ Vue/Day_81 State Management/product.vue
@@ -0,0 +1,17 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useProductStore = defineStore('product', () => {
+  const products = ref([])
+
+  // 외부에서 axios로 json 데이터 불러오기
+  // get() 내부에 문자열 형태로 url 작성
+  const fetchProducts = async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    products.value = res.data
+  }
+
+  
+
+  return { products, fetchProducts }
+})
